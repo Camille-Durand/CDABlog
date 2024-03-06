cc87070793b58aa12a1fe3420de280ecc178a595
Camille-Durand <camillefanguin@gmail.com>
2024-03-06 14:38:14 +0100

    Modification header site

--- src/Form/ArticleType.php
+++ src/Form/ArticleType.php
@@ -8,6 +8,7 @@ use App\Entity\User;
 use DateTimeInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -33,9 +34,12 @@ class ArticleType extends AbstractType
                 'required' => true
             ])
 
-            ->add('createAt', null, [
+            ->add('createAt', DateType::class, [
                 'widget' => 'single_text',
-                'attr' => ['class' => 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-96 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500']
+                'attr' => ['class' => 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-96 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'],
+                'label' => 'Date de création: ',
+                'label_attr' => ['class' => 'block mb-2 text-sm font-medium text-gray-900 dark:text-white'],
+                'required' => true
             ])
 
             ->add('imgArticle', TextType::class,[
